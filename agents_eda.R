21a06390aaff163198396e78616ceb4d2be03960
f-edwards <fedwards@uw.edu>
2023-11-21 10:21:18 -0800

    ff

--- agents_eda.R
+++ agents_eda.R
@@ -5,10 +5,28 @@ agents<-read_csv("./data/AGENTS_DATA_COLLECTION.csv")
 ### agency means tribe totals are contained in the single agency row, we can treat tribe numbers 
 ### zeroes for the purpose of agency / state totals
 agents<-agents %>% 
-  filter(YEAR >= 1880) %>% 
-  mutate(N_STUDENTS = ifelse(N_STUDENTS == "Agency" | N_STUDENTS == "Reserve"| 
-                               N_STUDENTS == "as above", NA, N_STUDENTS))
+  filter(YEAR >= 1895) %>% 
+  mutate(AVG_ATTENDANCE = ifelse(AVG_ATTENDANCE == "Agency" | AVG_ATTENDANCE == "Reserve"| 
+                               AVG_ATTENDANCE == "as above", NA, AVG_ATTENDANCE))
 
 
 sum_students <- agents %>% 
-  mutate(n_students = eval(parse(text = N_STUDENTS)))
+  mutate(AVG_ATTENDANCE = eval(parse(text = AVG_ATTENDANCE)))
+
+
+agents_1890<-agents %>% 
+  filter(YEAR>=1890) %>% 
+  select(`STATE/SUPERINTENDENCY`,
+         TRIBE,
+         AGENCY,
+         YEAR,
+         `DAY Y/N/B`,
+         AVG_ATTENDANCE) 
+
+
+students_ts<-agents_1890 %>% 
+  group_by(`STATE/SUPERINTENDENCY`, YEAR) %>% 
+  summarize(students = sum(AVG_ATTENDANCE))
+
+### use the flag on day school to sort into attendance categories
+### create AVG_ATTENDANCE, AVG_ATTENDANCE - DAY !="Y"
